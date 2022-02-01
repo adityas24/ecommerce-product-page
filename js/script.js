cb1aed62da8c076a1d4d632156a299f9451055dc
Aditya Singh <adisingh1519@gmail.com>
2022-02-01 08:49:17 +0530

    bug fixed

--- js/script.js
+++ js/script.js
@@ -3,6 +3,10 @@
 const minusIcon = document.querySelector(".icon-minus");
 const plusIcon = document.querySelector(".icon-plus");
 const productCount = document.querySelector(".product-count");
+const btn_atc = document.querySelector(".btn-atc");
+const basket = document.querySelector(".basket");
+const empty_cart = document.querySelector(".empty-cart");
+const cart_icon = document.querySelector('.cart-icon');
 let items = 0;
 
 minusIcon.addEventListener("click", () => {
@@ -15,6 +19,43 @@ minusIcon.addEventListener("click", () => {
 plusIcon.addEventListener("click", () => {
   items++;
   productCount.textContent = items;
+});
+
+btn_atc.addEventListener("click", () => {
+  empty_cart.style.display = "none";
+  basket.insertAdjacentHTML(
+    "beforeend",
+    `<div class="cart-item">
+              <img src="images/image-product-1.jpg" alt="" />
+              <div class="cart-item-description">
+                <h3>Fall Limited Edition Sneakers</h3>
+                <div class="cart-item-data">
+                  <div class="cart-item-price">$125.00</div>
+                  <div>x</div>
+                  <div class="cart-item-number">${items}</div>
+                  <div class="total-cost">$${(items * 125.0).toFixed(2)}</div>
+                </div>
+              </div>
+              <img
+                class="icon-delete"
+                src="images/icon-delete.svg"
+                alt="icon delete"
+              />
+            </div> 
+            <div class="btn-checkout-box">
+              <button class="btn-atc btn-checkout">Checkout</button>
+            </div> `
+  );
+
+  document.querySelector(".icon-delete").addEventListener("click", () => {
+    empty_cart.style.display = "block";
+    document.querySelector(".cart-item").style.display = "none";
+    document.querySelector(".btn-checkout-box").style.display = "none";
+  });
+});
+
+cart_icon.addEventListener('click', () => {
+  basket.classList.toggle('display-none');
 });
 
 
